8ba882b0088cb599ecdf1b975eb858e03d3b1412
vinhan16sep <vinhan16sep@gmail.com>
2019-05-31 00:06:55 +0700

    done

--- application/views/templates/member_parts/member_master_sidebar_view.php
+++ application/views/templates/member_parts/member_master_sidebar_view.php
@@ -7,7 +7,7 @@
             <div class="sidebar-brand-icon rotate-n-15">
                 <i class="fas fa-link"></i>
             </div>
-            <div class="sidebar-brand-text mx-3">Matching Admin <sup></sup></div>
+            <div class="sidebar-brand-text mx-3"> BMO Personal Page <sup></sup></div>
         </a>
 
         <!-- Divider -->
