c6c722575fe11934cc220ff4fa5b9ad4e1f781f7
vinhan16sep <vinhan16sep@gmail.com>
2019-06-01 04:07:49 +0700

    update

--- application/views/templates/admin_master_view.php
+++ application/views/templates/admin_master_view.php
@@ -29,9 +29,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <link rel="stylesheet"  href="<?php echo site_url('assets/'); ?>vendor/bootstrap-datepicker/css/bootstrap-datepicker3.css" />
 
     <link rel="stylesheet"  href="<?php echo site_url('assets/'); ?>scss/admin/style.css" />
+    <script src="<?php echo site_url('assets/'); ?>vendor/jquery/jquery-3.4.1.js"></script>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<!--    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
