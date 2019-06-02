c22b6ae37942aec141a4b04afe89c210bd6edbe7
vinhan16sep <vinhan16sep@gmail.com>
2019-06-03 02:13:47 +0700

    done

--- application/controllers/member/Matching.php
+++ application/controllers/member/Matching.php
@@ -17,6 +17,8 @@ class Matching extends Member_Controller {
         $this->load->model('event_model');
         $this->load->model('setting_model');
         $this->load->model('temp_register_model');
+        $this->load->model('users_model');
+        $this->load->model('category_model');
         $this->load->library('session');
         $this->load->helper('form');
 
@@ -36,12 +38,29 @@ class Matching extends Member_Controller {
             }
             $match_categories = $this->setting_model->get_matched_setting_data($event, $get['category_id'], $this->data['user']->id);
             foreach($match_categories as $key => $value){
-                $register_info = $this->temp_register_model->get_by_email_and_event_id($this->data['user']->email, $event);
+                $target_user = $this->users_model->fetch_by_id($value['user_id']);
+                $register_info = $this->temp_register_model->get_by_email_and_event_id($target_user['email'], $event);
                 $match_categories[$key]['register_info'] = $register_info;
             }
             $this->data['matched_setting'] = $match_categories;
         }
         $this->data['page_title'] = 'Tìm kiếm';
+        $category_root = $this->category_model->fetch_all_root_by_event($event);
+        $events = array();
+        if ($category_root) {
+            foreach ($category_root as $key => $value) {
+                $events[$value['id']]['name'] = $value['name'];
+                $category_sub = $this->category_model->fetch_all_sub_by_event_and_parent($event, $value['id']);
+                if ($category_sub) {
+                    foreach ($category_sub as $k => $val) {
+                        $events[$value['id']][$val['id']] = $val['name'];
+                    }
+
+                }
+            }
+        }
+
+        $this->data['events'] = $events;
         $this->render('member/matching/find_matching_view');
     }
 
