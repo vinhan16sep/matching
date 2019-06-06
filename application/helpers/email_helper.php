d2ea2cd9a7656e5598e5a95bc79d06a1002166e1
minhtruongmato <minhtruong93gtvt@gmail.com>
2019-06-07 02:04:33 +0700

    Thêm tổng quan doanh nghiệp cho tk user

--- application/helpers/email_helper.php
+++ application/helpers/email_helper.php
@@ -79,3 +79,70 @@ function email_template_look_account($data){
 
     return $message;
 }
+
+function send_mail_matching($email, $data, $matching = 'create', $role = 'admin'){
+    $mail = new PHPMailer();
+    $mail->IsSMTP(); // set mailer to use SMTP
+    $mail->Host = "smtp.gmail.com"; // specify main and backup server
+    $mail->Port = 465; // set the port to use
+    $mail->SMTPAuth = true; // turn on SMTP authentication
+    $mail->SMTPSecure = 'ssl';
+    $mail->Username = "support@vinasa.org.vn"; // your SMTP username or your gmail username
+    $mail->Password = "kcirpkmdlgbcobcv"; // your SMTP password or your gmail password
+    $from = "support@vinasa.org.vn"; // Reply to this email
+    $to = $email; // Recipients email ID
+    $name = 'http://bmo.vinasa.org.vn/'; // Recipient's name
+    $mail->From = $from;
+    $mail->FromName = $name; // Name to indicate where the email came from when the recepient received
+    $mail->AddAddress($to, $name);
+    $mail->CharSet = 'UTF-8';
+    $mail->WordWrap = 50; // set word wrap
+    $mail->IsHTML(true); // send as HTML
+    $mail->Subject = "Mail từ Matching Platform";
+
+    if ($matching == 'create') {
+        if ($role == 'admin') {
+            $mail->Body = email_template_matching_create_role_admin($data); //HTML Body
+        }else{
+            $mail->Body = email_template_matching_create($data); //HTML Body
+        }
+    }
+    if ($matching == 'approve') {
+        if ($role == 'admin') {
+            $mail->Body = email_template_matching_approve_role_admin($data); //HTML Body
+        }else{
+            $mail->Body = email_template_matching_approve($data); //HTML Body
+        }
+    }
+    if ($matching == 'reject') {
+        $mail->Body = email_template_matching_reject($data); //HTML Body
+    }
+
+    // $mail->SMTPDebug = 2;
+
+    if(!$mail->Send()){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function email_template_matching_create(){
+    return 'email hẹn gặp của member';
+}
+
+function email_template_matching_create_role_admin(){
+    return 'email hẹn gặp của member gửi cho admin';
+}
+
+function email_template_matching_approve(){
+    return 'email đồng ý hẹn gặp của member';
+}
+
+function email_template_matching_approve_role_admin(){
+    return 'email đồng ý hẹn gặp của member gửi cho admin';
+}
+
+function email_template_matching_reject(){
+    return 'email từ chối hẹn gặp của member';
+}
---
+++ application/views/member/overview/create.php
@@ -0,0 +1,45 @@
+<style type="text/css">
+    .error{
+        color: red;
+        position: relative;
+        line-height: 1;
+        width: 12.5rem;
+        font-size: 1rem !important;
+    }
+</style>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <?php if ($this->session->flashdata('message_error')): ?>
+                <div class="alert alert-danger alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                    <h4><i class="icon fa fa-warning"></i> Thông báo!</h4>
+                    <?php echo $this->session->flashdata('message_error'); ?>
+                </div>
+            <?php endif ?>
+            <h1 style="text-align: center;">Tổng quan về doanh nghiệp</h1>
+            <?php echo form_open_multipart('', array('class' => 'form-horizontal')); ?>
+            <div class="form-group">
+                <?php echo form_label('Tổng quát công nghệ sử dụng: ', 'overview'); ?>
+                <?php echo form_error('overview', '<div class="error">', '</div>'); ?>
+                <?php echo form_input('overview', '', 'class="form-control"'); ?>
+            </div>
+            <div class="form-group">
+                <?php echo form_label('Hồ sơ doanh nghiệp (Tóm tắt lĩnh vự hoạt động): ', 'profile'); ?>
+                <?php echo form_error('profile', '<div class="error">', '</div>'); ?>
+                <?php echo form_textarea('profile', '', 'class="form-control"'); ?>
+            </div>
+            <div class="form-group">
+                <?php echo form_label('File PDF: ', 'file'); ?>
+                <?php echo form_error('file', '<div class="error">', '</div>'); ?>
+                <?php echo form_upload('file', '', 'class=""'); ?>
+            </div>
+            <div class="form-group">
+                <?php echo form_submit('submit', 'Lưu Thông Tin', 'class="btn btn-primary"'); ?>
+                <!-- <a href="javascript:history.back()" name="back" class="btn btn-default btn-lg btn-block">Quay lại</a> -->
+            </div>
+
+            <?php echo form_close(); ?>
+        </div>
+    </div>
+</div>
