6077481eb755927e646f706a276577767cb1b2ce
minhtruongmato <minhtruong93gtvt@gmail.com>
2019-06-15 23:47:09 +0700

    template send mail

---
+++ application/views/auth/email_member/matching/email_template.tpl.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet"  href="<?php echo site_url('assets/'); ?>css/sb-admin-2.min.css" />
+	<style>
+        div{
+        	color: red;
+        }
+    </style>
+</head>
+<body>
+<div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out! <?php print_r($message); ?>
+</div>
+</body>
+</html>
