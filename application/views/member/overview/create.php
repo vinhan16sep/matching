aa6b058942e78b52a130d78e827c3c6d8218b972
minhtruongmato <minhtruong93gtvt@gmail.com>
2019-06-15 23:04:34 +0700

    update thông tin doanh nghiệp

--- application/views/member/overview/create.php
+++ application/views/member/overview/create.php
@@ -20,14 +20,49 @@
             <h6 style="text-align: center;">Tổng quan về doanh nghiệp</h6>
             <?php echo form_open_multipart('', array('class' => 'form-horizontal')); ?>
             <div class="form-group">
-                <?php echo form_label('Tổng quát công nghệ sử dụng: ', 'overview'); ?>
+                <?php echo form_label('Giới thiệu ngắn về Doanh nghiệp: ', 'overview'); ?>
                 <?php echo form_error('overview', '<div class="error">', '</div>'); ?>
                 <?php echo form_textarea('overview', '', 'class="form-control"'); ?>
             </div>
             <div class="form-group">
-                <?php echo form_label('Hồ sơ doanh nghiệp (Tóm tắt lĩnh vự hoạt động): ', 'profile'); ?>
+                <?php echo form_label('Lĩnh vực hoạt động: ', 'profile'); ?>
                 <?php echo form_error('profile', '<div class="error">', '</div>'); ?>
                 <?php echo form_textarea('profile', '', 'class="form-control"'); ?>
+            </div>
+            <div class="form-group">
+                <?php echo form_label('Sản phẩm/Giải pháp: ', 'product'); ?>
+                <?php echo form_error('product', '<div class="error">', '</div>'); ?>
+                <?php echo form_textarea('product', '', 'class="form-control"'); ?>
+            </div>
+            <div class="form-group">
+                <?php echo form_label('Thị trường chính hiện nay: ', 'market'); ?>
+                <?php echo form_error('market', '<div class="error">', '</div>'); ?>
+                <?php echo form_textarea('market', '', 'class="form-control"'); ?>
+            </div>
+            <div class="form-group">
+                <?php echo form_label('Đối tác chiến lược: ', 'partner'); ?>
+                <?php echo form_error('partner', '<div class="error">', '</div>'); ?>
+                <?php echo form_textarea('partner', '', 'class="form-control"'); ?>
+            </div>
+            <div class="form-group">
+                <?php echo form_label('Khách hàng tiêu biểu: ', 'customer'); ?>
+                <?php echo form_error('customer', '<div class="error">', '</div>'); ?>
+                <?php echo form_textarea('customer', '', 'class="form-control"'); ?>
+            </div>
+            <div class="form-group">
+                <?php echo form_label('Các chứng chỉ, bằng cấp đạt được (ISO, CMMI...): ', 'certificate'); ?>
+                <?php echo form_error('certificate', '<div class="error">', '</div>'); ?>
+                <?php echo form_textarea('certificate', '', 'class="form-control"'); ?>
+            </div>
+            <div class="form-group">
+                <?php echo form_label('Mong muốn hợp tác: ', 'desire'); ?>
+                <?php echo form_error('desire', '<div class="error">', '</div>'); ?>
+                <?php echo form_textarea('desire', '', 'class="form-control"'); ?>
+            </div>
+            <div class="form-group">
+                <?php echo form_label('Website: ', 'website'); ?>
+                <?php echo form_error('website', '<div class="error">', '</div>'); ?>
+                <?php echo form_input('website', '', 'class="form-control"'); ?>
             </div>
             <div class="form-group">
                 <?php echo form_label('File PDF: ', 'file'); ?>
